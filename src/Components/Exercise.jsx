b45ba458cd616e7c5eb626b0a1130da667c8b127
Michelle Ellis <michelle.ellis@shopify.com>
2020-10-31 09:53:04 -0400

    context compound component wip

--- src/Components/Exercise.jsx
+++ src/Components/Exercise.jsx
@@ -1,47 +1,34 @@
 import React from "react";
 import { Switch } from "../switch";
 
-const ToggleContext = React.createContext();
-ToggleContext.displayName = "ToggleContext";
-
-function Toggle({ children }) {
-  const [on, setOn] = React.useState(false);
-  const toggle = () => setOn(!on);
-
-  // 🐨 remove all this 💣 and instead return <ToggleContext.Provider> where
-  // the value is an object that has `on` and `toggle` on it.
-  return React.Children.map(children, (child) => {
-    return typeof child.type === "string"
-      ? child
-      : React.cloneElement(child, { on, toggle });
-  });
-}
-
-function ToggleOn({ children }) {
-  const { on } = useToggle();
+function ToggleOn({ on, children }) {
+  // const { on } = useToggle();
   return on ? children : null;
 }
 
-function ToggleOff({ children }) {
-  const { on } = useToggle();
+function ToggleOff({ on, children }) {
+  // const { on } = useToggle();
   return on ? null : children;
 }
 
-function ToggleButton({ ...props }) {
-  const { on, toggle } = useToggle();
+function ToggleButton({ on, toggle, ...props }) {
+  // const { on, toggle } = useToggle();
   return <Switch on={on} onClick={toggle} {...props} />;
 }
 
 function Exercise() {
+  const [on, setOn] = React.useState(false);
+  const toggle = () => setOn(!on);
+
   return (
     <div>
-      <Toggle>
-        <ToggleOn>The button is on</ToggleOn>
-        <ToggleOff>The button is off</ToggleOff>
-        <div>
-          <ToggleButton />
-        </div>
-      </Toggle>
+      {/* <Toggle> */}
+      <ToggleOn on={on}>The button is on</ToggleOn>
+      <ToggleOff on={on}>The button is off</ToggleOff>
+      <div>
+        <ToggleButton on={on} toggle={toggle} />
+      </div>
+      {/* </Toggle> */}
     </div>
   );
 }
